97bd3f3b143da5c66a6799331379bb804f4a6873
Ryan <rsully.dev+git@me.com>
2012-04-15 12:00:01 -0400

    Initial Commit

---
+++ SpringyPhysics/SPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPAppDelegate.h
+//  SpringyPhysics
+//
+//  Created by Ryan Sullivan on 4/15/12.
+//  Copyright (c) 2012 Freelance Web Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpringyPhysics/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpringyPhysics
+//
+//  Created by Ryan Sullivan on 4/15/12.
+//  Copyright (c) 2012 Freelance Web Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPAppDelegate class]));
+    }
+}
